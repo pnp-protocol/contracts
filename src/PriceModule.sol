e0f86fb9955229cfed642ab706e8db990c080fe6
proxima424 <proxima42ly@gmail.com>
2024-12-16 13:05:19 +0530

    complete priceDEX and bonding curve

--- src/PriceModule.sol
+++ src/PriceModule.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
+// ██████╗░███╗░░██╗██████╗░  ██████╗░██████╗░░█████╗░████████╗░█████╗░░█████╗░░█████╗░██╗░░░░░
+// ██╔══██╗████╗░██║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██║░░░░░
+// ██████╔╝██╔██╗██║██████╔╝  ██████╔╝██████╔╝██║░░██║░░░██║░░░██║░░██║██║░░╚═╝██║░░██║██║░░░░░
+// ██╔═══╝░██║╚████║██╔═══╝░  ██╔═══╝░██╔══██╗██║░░██║░░░██║░░░██║░░██║██║░░██╗██║░░██║██║░░░░░
+// ██║░░░░░██║░╚███║██║░░░░░  ██║░░░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝╚█████╔╝╚█████╔╝███████╗
+// ╚═╝░░░░░╚═╝░░╚══╝╚═╝░░░░░  ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░░╚════╝░░╚════╝░╚══════╝
+
 import {ITruthModule} from"./interfaces/ITruthModule.sol";
 
 // for uniswap V3 Pools
 contract PriceModule is ITruthModule {
 
     // Function to settle the market
-    function settle(bytes32 conditionId, uint256[] memory _marketParams) external override returns (bytes32 winnin) {
+    function settle(bytes32 conditionId, uint256[] memory _marketParams) external override returns (uint256 winningTokenId) {
         //  _winningTokenId = keccak256(abi.encodePacked(conditionId, _marketParams[0])); // Example logic
-
-        return _winningTokenId;
+        winningTokenId = uint256(keccak256(abi.encodePacked(conditionId, "YES")));
+        winningTokenId = uint256(keccak256(abi.encodePacked(conditionId, "NO")));
     }
 
 }

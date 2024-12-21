79d7b66808819ffe98336a76a284836c9257991d
proxima424 <proxima42ly@gmail.com>
2024-12-22 00:43:54 +0530

    init445

--- test/factory_test1.t.sol
+++ test/factory_test1.t.sol
@@ -5,15 +5,20 @@ import {Test, console2, Vm} from "../lib/forge-std/src/Test.sol";
 import {PNPFactory} from "../src/PNPFactory.sol";
 import {PythagoreanBondingCurve} from "../src/libraries/PythagoreanBondingCurve.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ITruthModule} from "../src/interfaces/ITruthModule.sol";
+import {IUniswapV3Pool} from "lib/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+import {PriceModule} from "../src/PriceModule.sol";
 
 contract FactoryTest is Test {
 
     uint256 public baseMainnetFork;
     PNPFactory public factory;
+    PriceModule public truthModule;
     
     address public collateralToken = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913; // USDC 
     address public bettingToken = 0x4200000000000000000000000000000000000006; // ETH 
-    address public pool = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
+    address public pool = 0xd0b53D9277642d899DF5C87A3966A349A798F224;
     // can be any token denominated Pool
     // ETH/USDT price
 
@@ -26,10 +31,17 @@ contract FactoryTest is Test {
 
         baseMainnetFork = vm.createFork("https://base-mainnet.public.blastapi.io");
         vm.selectFork(baseMainnetFork);
-        factory = new PNPFactory("bro");
+
         alice = makeAddr("alice");
         bob = makeAddr("bob");
         eve = makeAddr("eve");
+
+        vm.startPrank(eve); // eve is the factory admin
+        factory = new PNPFactory("bro");
+        truthModule = new PriceModule();
+        factory.setModuleAddress(0, address(truthModule));
+        vm.stopPrank();
+        
     
         vm.startPrank(richUSDC);
         IERC20(collateralToken).transfer(alice,5000*10**6);
@@ -63,17 +75,44 @@ contract FactoryTest is Test {
     }
 
     function test_marketCreation() public {
-        // // alice wants to create a market 
-        // // he wants to bet on price of ETH to be at a greater price 15 blocks later
+        // alice wants to create a market 
+        // he wants to bet on price of ETH to be at a greater price 15 blocks later
 
-        // // he provides iitial liquidity of 100 USDC
-        // vm.startPrank(alice);
-        // IERC20(collateralToken).approve(factory, 100*10**6);
+        // he provides iitial liquidity of 100 USDC
+        vm.startPrank(alice);
+        IERC20(collateralToken).approve(address(factory), 100*10**6);
 
-        // // now he calls our contract with config params
-        // address tokenInQuestion = bettingToken;
+        // now he calls our contract with config params
+        address tokenInQuestion = bettingToken; // eth  
+        uint256 initialLiquidity = 100*10**6; // 100 usdc
+        uint8 moduleId = 0; // price module
+        
+        uint256[] memory marketParams = new uint256[](2);
+        marketParams[0] = block.timestamp + 15; // 15 blocks later
 
-        // bytes32 conditionId = factory.createPredictionMarket(_initialLiquidity, _tokenInQuestion, _moduleId, _collateral, _marketParams, _pool);
+        // fetch current price from pool and bet a 2% increase 
+        uint256 currPrice = truthModule.getPrice(IUniswapV3Pool(pool));
+        uint256 targetPrice = currPrice + currPrice * 2 / 100;
+        marketParams[1] = targetPrice;
+
+        uint256 gasStart = gasleft();
+        bytes32 conditionId = factory.createPredictionMarket(initialLiquidity, tokenInQuestion, moduleId, collateralToken, marketParams, pool);
+        uint256 gasSpent = gasStart - gasleft();
+        console2.log("Prediction market created with conditionId: ", conditionId);
+        console2.log("Total gas spent in creating the market: ", gasSpent);
+        vm.stopPrank();
+
+        // we assert the mappings for the market 
+        assertEq(factory.moduleTypeUsed[conditionId], moduleId);
+        assertEq(factory.marketParams[conditionId][0], block.timestamp + 15);
+        assertEq(factory.marketParams[conditionId][1], targetPrice); 
+        assertEq(factory.marketSettled[conditionId], false);
+        assertEq(factory.marketReserve[conditionId], initialLiquidity);
+        assertEq(factory.winningTokenId[conditionId],0);
+
+        // check YES NO token balances of alice   
+
+        
     }
 
     function test_buyingDecisionTokens() public {}

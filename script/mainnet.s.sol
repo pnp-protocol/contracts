c9cfc2314d3716bc749ba8e8a8eaa99ab4a20968
proxima424 <proxima42ly@gmail.com>
2024-12-31 17:33:19 +0530

    :dizzy: :dizzy: :dizzy: :dizzy:

--- script/mainnet.s.sol
+++ script/mainnet.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {PNPFactory} from "../src/pnpFactory.sol";
+import {IFactory} from "../src/interfaces/IFactory.sol";
 import {PriceModule} from "../src/PriceModule.sol";
 import {Test, console2, Vm} from "../lib/forge-std/src/Test.sol";
 
@@ -13,25 +14,5 @@ contract MainnetScript is Script {
     function setUp() public {}
 
     function run() public {
-        // Retrieve the private key from environment variable
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        
-        // Start broadcasting transactions
-        vm.startBroadcast(deployerPrivateKey);
-
-        // Get reference to existing PNPFactory
-        PNPFactory factory = PNPFactory(FACTORY_ADDRESS);
-
-        // Deploy PriceModule
-        PriceModule priceModule = new PriceModule();
-        
-        // // Set PriceModule in factory with moduleId 0
-        // factory.setModuleAddress(0, address(priceModule));
-        
-        // vm.stopBroadcast();
-
-        // // Log the deployed addresses
-        console2.log("PriceModule deployed at:", address(priceModule));
-        // console2.log("Module set in factory at:", FACTORY_ADDRESS);
     }
 }
